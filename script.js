8c103978a1f963f8967a814808f36d08167eb701
VT_AOE_91 <amartin80108@gmail.com>
2022-05-21 19:49:29 -0400

    Attempted to create variables for list items > does NOT work.

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ var q2El = document.querySelector("#Q2");
 var q3El = document.querySelector("#Q3");
 var q4El = document.querySelector("#Q4");
 var q5El = document.querySelector("#Q5");
+var Q1wrong1El = document.querySelector(".Q1wrong1");
+var Q1wrong2El = document.querySelector(".Q1wrong2");
+var Q1wrong3El = document.querySelector(".Q1wrong3");
+var Q1correctEl = document.querySelector(".Q1correct");
 
 function startQuiz() {
     console.log("Quiz Initiated")
+}
+
+function consoleWrong() {
+    console.log("Wrong Answer!!!")
+}
+
+function consoleCorrect() {
+    console.log("Correct Answer!!!")
 }
 
 let timeout;
@@ -16,15 +28,22 @@ let timeout;
 // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
 
 
-function consoleReport() {
-    console.log("Time is Up!")
-}
+
 function Countdown() {
     introEl.style.display = "none";
     startEl.style.display = "none";
     var timeLeft = 5;
     
-    q1El.style.display = "block";
+    
+q1El.style.display = "block";
+
+Q1wrong1El.addEventListener("click",function(){
+    consoleWrong();
+});
+
+Q1correctEl.addEventListener("click", function() {
+    consoleCorrect();
+});
     
     var timeInterval = setInterval(function () {
     // As long as the `timeLeft` is greater than 1
